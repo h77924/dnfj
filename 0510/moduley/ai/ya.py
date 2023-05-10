9af0902e794aea8889a1d60bc0a0dee536496088
h77924 <65202590+h77924@users.noreply.github.com>
2023-05-10 16:24:41 +0900

    Add files via upload

---
+++ 0510/moduley/ai/ya.py
@@ -0,0 +1,11 @@
+
+#author:홍길동
+
+def hello10():
+    print("hello! selected!!! Ya.in ya py")
+
+if __name__ == "__main__" :
+    print("오늘은 5월 10일 입니다.")
+    hello10()
+    print("축제시작 곧 합니다.")
+    print("__name__ in 0510.py :",__name__)
---
+++ 0510/sam.py
@@ -0,0 +1,24 @@
+'''
+import random
+
+print("random.randint(10,100) ",random.randint(10,100))
+
+import math as m
+print(m.fsum([1,1,1,1,1,1,1]))
+print(m.pow(10,3))
+
+from math import pow as p
+print(p(10,3))
+
+#0510.py
+#김길동
+import ya
+print("============")
+ya.helloya()
+print("__name in ya.py :",__name__)
+
+
+from moduley.ai import ya as selected
+selected.hello10()
+'''
+
